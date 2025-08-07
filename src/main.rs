844d01608859df5f03216948cd06cb5791a5706c
Gert Vanthienen <gertv@anova.be>
2025-08-07 21:56:07 +0200

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,15 @@
 // probably the best hello world app I have ever seen
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
      println!("Hello, world!");
 }
+
+fn print_goodbye() {
+     println!("Goodbye, world!");
+}
+
