0d2bb004231623025a8339e80da950a3d794a3df
Gert Vanthienen <gertv@anova.be>
2025-08-08 09:14:06 +0200

    add comment to main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 // a 'hello world' program
 // probably the best hello world app I have ever seen
 
+// main method
 fn main() {
     print("Hello, world!");
 }
 
+// print helper method 
 fn print(m: &str) {
     println!("{m}")
 }
