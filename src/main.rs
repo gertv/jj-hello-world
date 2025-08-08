c5af93abdc6863a2683d504a57165816e721c6b6
Gert Vanthienen <gertv@anova.be>
2025-08-08 08:24:57 +0200

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,10 @@
 // probably the best hello world app I have ever seen
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-     println!("Hello, world!");
+fn print(m: &str) {
+    println!("{m}")
 }
-
-fn print_goodbye() {
-     println!("Goodbye, world!");
-}
-
